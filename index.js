2ba54f4cde9032e8257097d4446be36f2ec1d631
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-07-22 23:21:35 +0100

    send content mapped url encoded

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get('/json', (req, res) => {
-    res.status(200).json({hello: "world"});
-})
+app.get('/sections', (req, res) => {
+  res.status(200).json({
+    greeting: {
+      title: encodeURI('nice to meet you!'),
+      content: [
+        '👋 Hi, my name is Oliver Wilson and I am a machine that turns coffee into code a web developer.',
+        "I've created this website as a small project for me to experiment with new technologies and allow potential employers to gain an understanding of my skill set and interests.",
+        'The site is built using React, Redux (at the moment the only use case is a potentially over-engineered light switch), PostCSS, Webpack, Babel and has 100% unit test code coverage using the Jest testing framework.',
+        "The site is hosted using Github Pages and the domain is configured through a combination of Google Domains and Cloudflare. I've used a CICD workflow, following a git flow enabled approach towards my development. CirdleCI is used to automate the path-to-live for code changes to this website.",
+        'I soon hope to add automation tests using Cypress.'].map(paragraph => encodeURI(paragraph)),
+    },
+  });
+});
 
 app.get('/', (req, res) => {
-    res.status(200).send(`hello world!`);
-})
+  res.status(200).send('hello world!');
+});
 
 const portNumber = process.env.PORT || 3000;
 
