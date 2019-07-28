3d3bbc15b318d41062d5554b441c738ef4de3451
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-07-28 15:56:26 +0100

    fix: content was pointing to education section

--- sections/contact.js
+++ sections/contact.js
@@ -2,4 +2,5 @@ module.exports = {
   title: 'Get in Touch!',
   phone: '07580081310',
   email: 'contact@oliverwilson.dev',
+  content: [],
 };
--- sections/index.js
+++ sections/index.js
@@ -1,4 +1,4 @@
-const contact = require('./education');
+const contact = require('./contact');
 const education = require('./education');
 const experience = require('./experience');
 const greeting = require('./greeting');
