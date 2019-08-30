bd7292a97c575297508c43fd3414a78fb99c53a3
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-08-30 12:17:49 +0100

    feat: added more information

--- sections/education.js
+++ sections/education.js
@@ -7,5 +7,8 @@ module.exports = {
   content: [
     'I studied at the University of Kent for three years (2014 -2017) and obtained a first class degree with honours in Computer Science upon my graduation.',
     'During my studies I worked on a number of exciting front end projects and really enjoyed the creativity that developing software for the web afforded me.',
+    `It is fairly common for Computer Science students to participate in a sandwich-year/year in industry between their second and third years of studying. 
+    I chose to progress straight through to my third year and complete my degree within three years. I made this decision because I was thoroughly enjoying the course materials and had a strong self belief that I would find equally valuable experience upon graduating.`,
+    'During my final year at University I worked as a consultant for an IT consultancy. I worked with a number of clients to create bespoke web-based solutions to suit their needs.',
   ],
 };
--- sections/greeting.js
+++ sections/greeting.js
@@ -8,7 +8,8 @@ module.exports = {
   },
   content: [
     "I've created this website as a small project for me to experiment with new technologies and allow potential employers to gain an understanding of my skill set and interests.",
-    'The site is built using React, Redux (at the moment the only use case is a potentially over-engineered light switch), PostCSS, Webpack, Babel and has 100% unit test code coverage using the Jest testing framework.',
+    'The site is built using React, Redux (to manage light/dark theme changes and to asynchronously load the content for the website), PostCSS, Webpack, Babel and has 100% unit test code coverage using the Jest testing framework, complimented by Enzyme.',
     "The site is hosted using Github Pages and the domain is configured through a combination of Google Domains and Cloudflare. I've used a CICD workflow, following a git flow enabled approach towards my development. CirdleCI is used to automate the path-to-live for code changes to this website.",
-    'I soon hope to add automation tests using Cypress.'],
+    'This site stores cookie data based on the theme selector if you choose to use it. A cookie is written detailing the theme choice which enables the site to remember your theme selection and preserve that selection when you refresh the page.',
+    'I aim to support all modern browsers for this project however, I had an internal battle with myself over choosing the use of the javascript Proxy (which is not supported by IE11 and older versions of modern browsers and can also not be pollyfilled) or supporting all browsers. I decided that I wanted to explore the use of javascript proxies, so please excuse your UX if you attempt to view this site on an unsupported browser.'],
 };
