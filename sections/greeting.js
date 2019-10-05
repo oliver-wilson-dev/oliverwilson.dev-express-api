e680ee46f272443c8f3158fe8519d592a901eeb4
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-10-05 10:01:15 +0100

    refactor: update greeting

--- sections/greeting.js
+++ sections/greeting.js
@@ -7,7 +7,7 @@ module.exports = {
     end: ' a web developer.',
   },
   content: [
-    "I've created this website as a small project for me to experiment with new technologies and allow potential employers to gain an understanding of my skill set and interests.",
+    "I've created this website as a small project for me to experiment with new technologies and to showcase my skill set and interests.",
     'The site is built using React, Redux (to manage light/dark theme changes and to asynchronously load the content for the website), PostCSS, Webpack, Babel and has 100% unit test code coverage using the Jest testing framework, complimented by Enzyme.',
     "The site is hosted using Github Pages and the domain is configured through a combination of Google Domains and Cloudflare. I've used a CICD workflow, following a git flow enabled approach towards my development. CirdleCI is used to automate the path-to-live for code changes to this website.",
     'This site stores cookie data based on the theme selector if you choose to use it. A cookie is written detailing the theme choice which enables the site to remember your theme selection and preserve that selection when you refresh the page.',
