0e4a2678455d9a02535779fb4d64a42896d59222
Oliver Wilson <oliver.wilson@asos.com>
2020-01-12 19:13:37 +0000

    feat: add space between dates for university studies

--- sections/education.js
+++ sections/education.js
@@ -5,7 +5,7 @@ module.exports = {
   courseName: { precedingInformation: 'Bachelor of Science in', name: 'Computer Science' },
   degreeClassification: { precedingInformation: 'Classification', name: 'first' },
   content: [
-    'I studied at the University of Kent for three years (2014 -2017) and obtained a first class degree with honours in Computer Science upon my graduation.',
+    'I studied at the University of Kent for three years (2014 - 2017) and obtained a first class degree with honours in Computer Science upon my graduation.',
     'During my studies I worked on a number of exciting front end projects and really enjoyed the creativity that developing software for the web afforded me.',
     `It is fairly common for Computer Science students to participate in a sandwich-year/year in industry between their second and third years of studying. 
     I chose to progress straight through to my third year and complete my degree within three years. I made this decision because I was thoroughly enjoying the course materials and had a strong self belief that I would find equally valuable experience upon graduating.`,
